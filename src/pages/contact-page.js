44f637084c0a2cfdb8ab21818c19553e27f088ce
Jlush <jstewart@smarttech.com>
2020-03-05 10:05:33 +0200

    Updating the contact us page

--- src/pages/contact-page.js
+++ src/pages/contact-page.js
@@ -12,11 +12,11 @@ const ContactPage = () => (
     <Container className="contact-page px-0">
       <Row>
         <Col className="justify-content-center my-3">
-          <h1 className='text-center'>Contact Us</h1>
-          <p className='text-center'>
-            Need Help? Then please contact with us,
+          <h1 className="text-center">Contact Me</h1>
+          <p className="text-center">
+            Need Help? Then please contact me,
             <br />
-            We will get back to you As soon As possible
+            I will get back to you As soon As possible.
           </p>
           <div className="element-divider">
             <span className="elementor-divider-separator"></span>
@@ -24,17 +24,19 @@ const ContactPage = () => (
         </Col>
       </Row>
       <Row>
-        <Col>
-          <p className='bio'>
-            Lorem ipsum dolor sit amet, consectetu sicing. Iste ratione
-            molestias. Lorem ipsum dolor sit amet, consectetur adipisicing.
-            Ratione molesti magni risus.
+        <Col className="bio-column">
+          <p className="bio">
+            I am a software engineer, I have experience in a wide variety of
+            technologies from JavaScript web application development to custom
+            Wordpress websites. I would love to help you develop a website, get intouch!
           </p>
           <h2>Address</h2>
           <p>Durban, South Africa</p>
           <h2>Email</h2>
-          <p>info@joshuastewart.design <br/>
-          www.joshuastewart.design</p>
+          <p>
+            info@joshuastewart.design <br />
+            www.joshuastewart.design
+          </p>
         </Col>
         <Col>
           <ContactForm />
